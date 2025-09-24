95f34347e2a696626feff6f4194b0ac0d45b1367
valeagro <eliabelc2@gmail.com>
2025-09-24 08:09:30 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'agrovvale-os-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './Logo-Site-Agrovale.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache pre-preenchido');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
